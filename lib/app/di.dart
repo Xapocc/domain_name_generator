1cfa15210d1269f580311d1089feff1fc4b07474
developer <dev@dev>
2022-05-25 14:24:13 +0300

    change: moved firebase instance to get_it di

--- lib/app/di.dart
+++ lib/app/di.dart
@@ -1,9 +1,11 @@
 import 'package:domain_name_generator/data/repositories/impl_repository_firebase.dart';
 import 'package:domain_name_generator/domain/use_cases/use_case_dictionary_firebase.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:get_it/get_it.dart';
 
 class DependencyInjection {
   static void setupGetIt() {
+    GetIt.I.registerSingleton<FirebaseDatabase>(FirebaseDatabase.instance);
     GetIt.I.registerSingleton<UseCaseDictionaryFirebase>(
         UseCaseDictionaryFirebase(repository: ImplRepositoryFirebase()));
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
 import 'package:domain_name_generator/app/app_root.dart';
 import 'package:domain_name_generator/app/di.dart';
-import 'package:domain_name_generator/domain/entities/entity_zone.dart';
-import 'package:domain_name_generator/domain/use_cases/use_case_dictionary_firebase.dart';
 import 'package:flutter/material.dart';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
-import 'package:get_it/get_it.dart';
 import 'firebase_options.dart';
 
 late FirebaseDatabase database; // TODO: move to DI
@@ -14,12 +11,11 @@ late FirebaseDatabase database; // TODO: move to DI
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  DependencyInjection.setupGetIt();
-
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  database = FirebaseDatabase.instance;
+
+  DependencyInjection.setupGetIt();
 
   runApp(const AppRoot());
 }
